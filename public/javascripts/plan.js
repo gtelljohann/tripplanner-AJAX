4b6edafc0d51aacca2011e2db41fc262d24ffe2e
Griffin <g.telljohann@gmail.com>
2014-12-03 10:45:23 -0500

    copied work on hotel id things to work with activities and restaurants

--- public/javascripts/plan.js
+++ public/javascripts/plan.js
@@ -47,10 +47,8 @@ function addThing() {
 		writeVisitToServer(thisHotel_id, currentDay, "hotel");
 	});
 
-	$("#activity-picker").submit(function(e) {
-		var activity = $("#activity-picker option:selected").text();
+	function addActivity (activity, activity_id){
 		var location = finder(activity, all_activities);
-		e.preventDefault();
 		$("#activity-list").append("<li class='list-group-item'><span>" + activity + "</span> " + deleteButton + "</li>");
 		var marker = new google.maps.Marker({
 			position: new google.maps.LatLng(location[0], location[1]),
@@ -59,12 +57,17 @@ function addThing() {
 		});
 		plan[currentDay].Activities.push({"name": activity, "marker":marker});
 		setDeleteButton("#activity-list", "Activities");
+	}
+	$("#activity-picker").submit(function(e) {
+		var thisActivity = $("#activity-picker option:selected").text();
+		var thisActivity_id = $("#activity-picker option:selected").attr("id").replace(/"/g, '');
+		e.preventDefault();
+		addActivity(thisActivity, thisActivity_id);
+		writeVisitToServer(thisActivity_id, currentDay, "activity");
 	})
 
-	$("#restaurant-picker").submit(function(e) {
-		var restaurant = $("#restaurant-picker option:selected").text();
+	function addRestaurant(restaurant, restaurant_id) {
 		var location = finder(restaurant, all_restaurants);
-		e.preventDefault();
 		$("#restaurant-list").append("<li class='list-group-item'><span>" + restaurant + "</span> " + deleteButton + "</li>");
 		var marker = new google.maps.Marker({
 			position: new google.maps.LatLng(location[0], location[1]),
@@ -73,6 +76,14 @@ function addThing() {
 		});
 		plan[currentDay].Restaurants.push({"name": restaurant, "marker":marker});
 		setDeleteButton("#restaurant-list", "Restaurants");
+	}
+
+	$("#restaurant-picker").submit(function(e) {
+		var thisRestaurant = $("#restaurant-picker option:selected").text();
+		var thisRestaurant_id = $("#restaurant-picker option:selected").attr("id").replace(/"/g, '');
+		e.preventDefault();
+		addRestaurant(thisRestaurant, thisRestaurant_id);
+		writeVisitToServer(thisRestaurant_id, currentDay, "restaurant");		
 	})
 
 }
@@ -96,19 +107,19 @@ function getDays() {
 }
 
 function writeVisitToServer(attraction_id, dayId, type_of_place) {
-  var post_data = {
-    attraction_id: attraction_id,
-    attraction_type: type_of_place
-  };
- 
+	var post_data = {
+		attraction_id: attraction_id,
+		attraction_type: type_of_place
+	};
+
   // the callback function below will be called if this request completes successfully. 
   // the server's response to this request is passed into this callback function as "responseData"
- 
+
   var post_callback = function (responseData) {
     //... what to do when done...
     alert("We added the thing!");
-  };
- 
+};
+
   // jQuery Ajax call
   $.post( "/days/" + dayId + "/attractions", post_data, post_callback);
 }
